cf2185c7582083f598529d27c6f06e95d37ca98e
Merray <j.merray@free.fr>
2026-04-13 16:11:26 +0200

    Modification de l'icone, du splash screen, du nom

--- fakeData/fakeQuotes.js
+++ fakeData/fakeQuotes.js
@@ -3,4 +3,10 @@ export const FakeQuotes = [
   "Citation : « La cuisine, c’est le cœur de la maison. » – Paul Bocuse",
   "Astuce : Ajoutez une pincée de sucre dans votre sauce tomate pour réduire l’acidité.",
   "Citation : « La gourmandise commence quand on n’a plus faim. » – Brillat-Savarin",
+  "Astuce : Placez une cuillère en bois au-dessus d'une casserole d'eau bouillante pour éviter qu'elle ne déborde.",
+  "Astuce : Pour râper plus facilement vos légumes et crudités, enduisez votre râpe d'huile afin que ceux-ci glissent naturellement.",
+  "Astuce : Si votre salade est défraîchie, trempez-la dans de l'eau sucrée pendant une heure, elle retrouvera tout son croquant.",
+  "Astuce : Pour stopper la cuisson des œufs à la coque et obtenir la consistance parfaite, placez-les immédiatement dans de l'eau glacée une fois sortis de l'eau bouillante.",
+  "Astuce : Une fois votre viande rouge cuite, laissez-la reposer quelques minutes dans un plat recouvert de papier aluminium. Les sucs pénètrent ainsi mieux les tissus, ce qui rend la viande tendre et juteuse.",
+  "Astuce : Assaisonnez votre viande rouge après et pas avant la cuisson, au risque de voir brûler le poivre dans la poêle. Seul le sel peut être ajouté avant et pendant la cuisson, puisqu'il fait ressortir les sucs."
 ];
