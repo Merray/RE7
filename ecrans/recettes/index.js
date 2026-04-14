b71c5398ced0bd1cd7adffa9751eb17535a2a4e7
Merray <j.merray@free.fr>
2026-04-14 22:28:53 +0200

    Modification du logo et splash plus propre + bouton d'ajout

--- ecrans/recettes/index.js
+++ ecrans/recettes/index.js
@@ -1,12 +1,14 @@
-import { View, Text, FlatList, ScrollView } from 'react-native'
+import { View, Text, FlatList, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { FakeRecettes } from '../../fakeData/fakeRecettes'
 import recetteStyle from './style'
 import RecetteComposant from '../../composants/recetteComposant'
 import HeaderComposant from '../../composants/headerComposant';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Recettes = ({ navigation }) => {
   return (
+    <View style={{ flex: 1 }}>
       <FlatList
         data={FakeRecettes}
         keyExtractor={item => item.id}
@@ -17,6 +19,16 @@ const Recettes = ({ navigation }) => {
             <RecetteComposant item={item} navigation={navigation} />
           )
         }} />
+
+        /* Bouton pour ajouter */
+        <TouchableOpacity
+        style={recetteStyle.fab}
+        activeOpacity={0.8} // Pour effet de pression
+        onPress={() => navigation.navigate('recetteFormulaire')}
+      >
+        <MaterialCommunityIcons name="plus" size={35} color="white" />
+      </TouchableOpacity>
+    </View>
   )
 }
 
--- ecrans/recettes/style.js
+++ ecrans/recettes/style.js
@@ -6,7 +6,25 @@ const recetteStyle = StyleSheet.create({
         backgroundColor: COULEURS.secondary,
         paddingHorizontal: PADDING.horizontal,
         paddingVertical: PADDING.vertical
-    }
+    },
+    fab: {
+  position: 'absolute',
+  bottom: 20,
+  right: 20,
+  width: 60,
+  height: 60,
+  borderRadius: 30,
+  backgroundColor: '#05a565',
+  justifyContent: 'center',
+  alignItems: 'center',
+  elevation: 8,
+},
+
+fabText: {
+  color: 'white',
+  fontSize: 30,
+  fontWeight: 'bold',
+},
 })
 
 export default recetteStyle
